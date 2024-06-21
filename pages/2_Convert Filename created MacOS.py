ab2c3b5ad0df3463c7e4c523536228997a4093e3
geunsu-son <gnsu0705@gmail.com>
2024-06-21 15:36:14 +0900

    Update blog link

--- pages/2_Convert Filename created MacOS.py
+++ pages/2_Convert Filename created MacOS.py
@@ -14,7 +14,7 @@ st.sidebar.write(
     ### 연락처
     📞 Tel. 010-4430-2279  
     📩 E-mail. [gnsu0705@gmail.com](gnsu0705@gmail.com)  
-    💻 Blog. [Super-Son](https://super-son.tistory.com/)
+    💻 Blog. [Super-Son](https://super-son.tistory.com/)  
     😎 Resume. [Super-Son](https://super-son-resume.streamlit.app)
     """
 )
