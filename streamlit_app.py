1038649f292e20d275e63554aa3f05ae44a2260b
SUPER-SON <168807522+geunsu-son@users.noreply.github.com>
2025-06-29 02:17:34 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,6 @@ with st.sidebar:
     📞 Tel. 010-4430-2279  
     📩 E-mail. [gnsu0705@gmail.com](gnsu0705@gmail.com)  
     💻 Blog. [Super-Son](https://super-son.tistory.com)  
-    😎 Resume. [Super-Son](https://super-son-resume.streamlit.app)
     """
     )
 
